75725b8fc1af7d0b78279f44349420656c5ff40e
LynasTing <LynasTing@proton.me>
2024-04-28 18:19:20 +0800

    -ci: JSX基础学习

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+function getName() {
+  return '南京'
+}
+const LynasTing = {
+  learn: () => {
+    return 'jack'
+  }
+}
 
-function App() {
+/**
+ * 识别js表达式
+ */
+function App1() {
+  const count = 99
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* 使用引号传递字符串 */}
+      <div>{'相信未来，保持李志'}</div>
+      {/* 使用JS变量 */}
+      <div>{count}</div>
+      {/* 函数调用和方法调用 */}
+      <div>{getName()}</div>
+      <div>{LynasTing.learn()}</div>
+      {/* 使用JS对象 */}
+      <div style={{color: 'red'}}>I'm a red div.</div>
     </div>
   );
 }
+
+/**
+ * 实现列表渲染
+ */
+function App2() {
+  const list = [
+    { id: 101, name: 'Vue' },
+    { id: 102, name: 'React' },
+    { id: 103, name: 'Angular' }
+  ]
+  return (
+    <div className="App">
+      <ul>{list.map(item => <li key={item.id}>{item.name}</li>)}</ul>
+    </div>
+  )
+}
+
+/**
+ * 条件渲染
+ */
+function App3() {
+  const flag = false
+  return (
+    <div className="App">
+      {/* 逻辑与 && */}
+      <div>{flag && '欢迎使用'}</div>
+      {/* 三元表达式 */}
+      <div>{flag ? '您已登录' : '请登录'}</div>
+    </div>
+  )
+}
+
+/**
+ * 多种条件渲染
+ */
+function App4() {
+  const articleType = 3  // 0 1 3
+  function getArticleTem() {
+    if(articleType === 0) {
+      return <div>我是无图文章</div>
+    }else if(articleType === 1) {
+      return <div>我是单图模式</div>
+    }else {
+      return <div>我是三图模式</div>
+    }
+  }
+  return (
+    <div className="App">
+      {getArticleTem()}
+    </div>
+  )
+}
+
+/**
+ * 事件绑定
+ */
+function App( ) {
+  // 事件默认参数 e
+  // const handleClick = (e) => {
+  //   console.log(`e + ::>>`, e)
+  // }
+
+  // 自定义传参
+  // const handleClick = (name) => {
+  //   console.log(`name + ::>>`, name)
+  // }
+
+  // 同时传事件默认参数和自定义传参
+  const handleClick = (e, name) => {
+    console.log(`e + ::>>`, e)
+    console.log(`name + ::>>`, name)
+  }
+  return (
+    <div className="App">
+      <div onClick={(e) => handleClick(e, 'Lynas')}>click me</div>
+    </div>
+  )
+}
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
+// index.js 是项目的入口，整个项目从这里开始运行
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+// App 是项目的根组件
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// 渲染根组件到#root
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
