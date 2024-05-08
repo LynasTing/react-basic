6a3a22d00ec8c970a5f2c857fbd934a81f9a4d5a
LynasTing <LynasTing@proton.me>
2024-05-08 10:56:42 +0800

    -ci: TabBar 功能实现

--- src/day_04/bill/page/Layout/index.js
+++ src/day_04/bill/page/Layout/index.js
@@ -1,23 +1,54 @@
-import { Outlet } from 'react-router-dom'
-import { Button } from 'antd-mobile'
+import { Outlet, useNavigate } from 'react-router-dom'
 import { getBillList } from '@/day_04/bill/store/modules/billStore'
 import { useDispatch } from 'react-redux' 
 import { useEffect } from 'react'
+import './index.scss'
+import { TabBar } from 'antd-mobile'
+import { BillOutline, CalculatorOutline, AddCircleOutline } from 'antd-mobile-icons'
+
+const tabs = [
+  {
+    key: '/',
+    title: '月度账单',
+    icon: <BillOutline />
+  },
+  {
+    key: '/new',
+    title: '记账',
+    icon: <AddCircleOutline />
+  },
+  {
+    key: '/year',
+    title: '年度账单',
+    icon: <CalculatorOutline />
+  },
+]
 
 const Layout = () => {
   const dispatch = useDispatch()
+  /**
+   * 获取账单信息
+   */
   useEffect(() => {
     dispatch(getBillList())
   }, [dispatch])
+  
+  const navigate = useNavigate() 
+  /**
+   * 切换路由
+   */
+  const handleChangeRouter = (path) => {
+    navigate(path)
+  }
   return (
-    <div>
-      <p>I'm Layout</p>
-      <div>
-        <Button color='primary'>Test global theme</Button>
-        <div className='purple'>
-          <Button color='primary'>Test partial theme</Button>
-        </div>
+    <div className='layout'>
+      <div className='container'>
         <Outlet />
+      </div>
+      <div className='footer'>
+        <TabBar onChange={handleChangeRouter}>
+          {tabs.map(item => (<TabBar.Item key={item.key} icon={item.icon} title={item.title} />))}
+        </TabBar>
       </div>
     </div>
   )
