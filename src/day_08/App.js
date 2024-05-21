a2250e231d05a35d7542f4715c7e70bd97856e0a
LynasTing <LynasTing@proton.me>
2024-05-21 17:49:42 +0800

    -ci: zustand切片模式

--- src/day_08/App.js
+++ src/day_08/App.js
@@ -2,19 +2,31 @@ import { create } from 'zustand'
 import { Button } from 'antd'
 import { useEffect } from 'react'
 
-const useStore = create(set => ({
-  // 定义状态
-  count: 1,
-  channels: [],
-  // 修改状态的方法
-  inc: () => set(state => ({ count: state.count + 1 })),
-  fetchChannels: async () => {
-    const res = await fetch('http://geek.itheima.net/v1_0/channels')
-    console.log(`res + ::>>`, res)
-    const jsonData = await res.json()
-    console.log(`jsonData + ::>>`, jsonData)
-    set({ channels: jsonData.data.channels})
+const counterStore = (set => {
+  return {
+    // 定义状态
+    count: 1,
+    // 修改状态的方法
+    inc: () => set(state => ({ count: state.count + 1 }))
   }
+})
+
+const channelStore = (set => {
+  return {
+    // 定义状态
+    channels: [],
+    // 修改状态的方法
+    fetchChannels: async () => {
+      const res = await fetch('http://geek.itheima.net/v1_0/channels')
+      const jsonData = await res.json()
+      set({ channels: jsonData.data.channels})
+    }
+  }
+}) 
+
+const useStore = create((...a) => ({
+  ...counterStore(...a),
+  ...channelStore(...a)
 }))
 
 function App() {
